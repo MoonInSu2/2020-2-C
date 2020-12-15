eb18e3c671209ad8465517b03561ef6bf171103d
MoonInSu2 <75931013+MoonInSu2@users.noreply.github.com>
2020-12-15 18:49:15 +0900

    수정 upload

---
+++ code/ch07/whilebasic.c
@@ -0,0 +1,14 @@
+// file : whilebasic.c
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("C 언어 재미있네요!\n");
+	printf("C 언어 재미있네요!\n");
+	printf("C 언어 재미있네요!\n");
+	printf("\n");
+	printf("제어변수 count => 4");
+
+	return 0;
+}
